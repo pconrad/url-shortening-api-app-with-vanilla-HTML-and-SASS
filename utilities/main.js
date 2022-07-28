1b8c6fcabea4a3c72a361a59358a264b2daddd7e
CodingCocho <raulujr@gmail.com>
2022-07-28 15:23:41 -0700

    Styled mobile layout

--- utilities/main.js
+++ utilities/main.js
@@ -1,2 +1,25 @@
-const url 
-const c
+const mobileHero = document.getElementById("hero-image")
+const mobileNavContainer = document.getElementById("mobile-container")
+const hamburgerIconOpen = document.getElementById("hamburger-icon-open");
+const hamburgerIconClose = document.getElementById("hamburger-icon-close");
+
+console.log(mobileHero)
+console.log(mobileNavContainer)
+console.log(hamburgerIconOpen)
+console.log(hamburgerIconClose)
+
+const toggleOpen = () =>
+{
+    hamburgerIconOpen.style.display = "none";
+    hamburgerIconClose.style.display = "block";
+    mobileHero.style.display = "none";
+    mobileNavContainer.style.display = "block";
+}
+
+const toggleClose = () =>
+{
+    hamburgerIconOpen.style.display = "block";
+    hamburgerIconClose.style.display = "none";
+    mobileHero.style.display = "block";
+    mobileNavContainer.style.display = "none";
+}
