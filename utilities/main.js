201ee3f5dddbed9ac46cab5097514eae429e1138
CodingCocho <raulujr@gmail.com>
2022-08-01 20:37:28 -0700

    Finished implementing API.

--- utilities/main.js
+++ utilities/main.js
@@ -24,12 +24,48 @@ const toggleClose = () =>
     mobileNavContainer.style.display = "none";
 }
 
+const urlInputContainer =document.getElementById("url-input-container")
 const urlInput = document.getElementById("url");
-const statsSection = document.getElementById("stats-section")
+const listOfUrls = document.getElementById("url-list")
+const error = document.getElementById("error")
+let firstLink = true;
+
+const validateUrl = (urlContainer) =>
+{
+    let urlString = urlInput.value;
+    if(urlString.includes(".com") || urlString.includes(".net") || 
+    urlString.includes(".gov") || urlString.includes(".org"))
+    {
+        urlString = urlString.slice(urlString.indexOf("."), urlString.length);
+        console.log(urlString)
+        if(urlString === ".com" || urlString === ".net" ||
+        urlString === ".gov" || urlString === ".org")
+        {
+            urlInputContainer.style.border = "none";
+            error.innerHTML = "";
+        }
+        else
+        {
+            urlInputContainer.style.border = "1px solid red";
+            error.innerHTML = "Please add a valid link";
+        }
+    }
+     else if(urlString === "")
+    {
+        urlInputContainer.style.border = "1px solid red";
+        error.innerHTML = "Please add a link";
+    }
+    else
+    {
+        urlInputContainer.style.border = "1px solid red";
+        error.innerHTML = "Please add a valid link";
+    }
+}
 
 const shortenUrl = async () =>
 {
-    const urlString = urlInput.value;
+    let urlString = urlInput.value;
+    
     let urlResponse = await fetch(`https://api.shrtco.de/v2/shorten?url=${urlString}`)
     try 
     {
@@ -41,19 +77,37 @@ const shortenUrl = async () =>
     }
     catch(error)
     {
-        console.log(error);
         return;
     }
     let urlJson = await urlResponse.json();
     let urlObject = urlJson.result;
-    console.log(urlObject);
+    createUrlBlock(urlObject);
 }
 
-
-
-const createUrlBlock = () =>
+let clone = document.getElementById("url-container").cloneNode(true);
+const createUrlBlock = (urlObject) =>
 {
-    console.log(statsSection.children)
+    let urlBlock = document.getElementById("url-container")
+    if(firstLink)
+    {
+        urlBlock.style.display = "flex";
+        firstLink = false;
+    }
+    else
+    {
+        urlBlock = clone.cloneNode(true);
+        urlBlock.style.marginTop = "0px";
+        listOfUrls.appendChild(urlBlock);
+    }
+    urlBlock.children[0].innerHTML = urlObject.original_link;
+    urlBlock.children[1].children[0].innerHTML = urlObject.short_link;
+    urlBlock.style.display = "flex";
 }
 
-createUrlBlock();
+const copyLink = (button) =>
+{
+    let urlContainer = button.parentNode;
+    navigator.clipboard.writeText(urlContainer.children[0].innerHTML);
+    button.innerHTML = "Copied!";
+    button.style.backgroundColor = "hsl(257, 27%, 26%)";
+}
